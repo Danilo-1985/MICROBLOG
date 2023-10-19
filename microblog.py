8064346b3365399689bac2d1f477c6f8dc0db3f4
Danilo-1985 <134429134+Danilo-1985@users.noreply.github.com>
2023-10-19 00:56:33 -0300

    Segunda tentativa de hospedar no Heroku

--- microblog.py
+++ microblog.py
@@ -2,7 +2,7 @@ from app import app
 import os
 
 if __name__ == 'main':
-    port = int(os.gentenv('PORT'), '5000')
+    port = int(os.getenv('PORT'), '5000')
     app.run(host='0.0.0.0', port=port)
 
 
