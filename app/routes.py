055ab217de9fb4305aedba5ea623c4f77e999041
Danilo-1985 <134429134+Danilo-1985@users.noreply.github.com>
2023-10-19 00:23:09 -0300

    Initial commit

---
+++ app/routes.py
@@ -0,0 +1,14 @@
+from app import app
+from flask import render_template
+
+
+@app.route("/")
+@app.route("/index")
+def index():
+    return render_template('index.html')
+
+
+@app.route("/contato")
+def contact():
+    return render_template('contato.html')
+
